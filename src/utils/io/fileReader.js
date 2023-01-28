bab006cbc31fa6576c36c14b0ae94abe52b379fb
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-01-28 15:46:01 -0300

    feat: parse JSON

--- src/utils/io/fileReader.js
+++ src/utils/io/fileReader.js
@@ -1,6 +1,8 @@
 const fs = require('fs/promises');
 
-const readFileAsync = async (path) => fs.readFile(path, 'utf-8');
+const readFileAsync = async (path) => {
+    return JSON.parse(await fs.readFile(path, 'utf-8'));
+}
 
 module.exports = {
     readFileAsync
