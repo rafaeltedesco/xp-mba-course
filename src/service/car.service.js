e28630499c2fde64d698f9f0e2b58090226f2401
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-01-28 16:49:21 -0300

    feat: get n cars with more models and get n cars with less models

--- src/service/car.service.js
+++ src/service/car.service.js
@@ -1,13 +1,33 @@
 const { carHelper } = require('../helpers');
 const { carRepository } = require('../repository');
 
-const getBrandNamesWithMoreCarsModels = async () => 
-    carHelper.getMax(carHelper.countCarsModels(await carRepository.getAllCars())).map(car => car.brand);
+const getBrandNamesWithMoreCarsModels = async () =>
+  carHelper
+    .getMax(carHelper.countCarsModels(await carRepository.getAllCars()))
+    .map((car) => car.brand);
 
-const getBrandNamesWithLessCarsModels = async () => 
-    carHelper.getMin(carHelper.countCarsModels(await carRepository.getAllCars())).map(car => car.brand);
+const getBrandNamesWithLessCarsModels = async () =>
+  carHelper
+    .getMin(carHelper.countCarsModels(await carRepository.getAllCars()))
+    .map((car) => car.brand);
+
+const getNCarsBrandNamesWithMoreCarsModels = async (numberOfBrands) => {
+  return carHelper
+    .countCarsModels(await carRepository.getAllCars())
+    .sort(({ qtdModels: carA }, { qtdModels: carB }) => carB - carA)
+    .slice(0, numberOfBrands).map(({ brand, qtdModels }) => `${brand} - ${qtdModels}`);
+};
+
+const getNCarsBrandNamesWithLessCarsModels = async (numberOfBrands) => {
+    return carHelper
+      .countCarsModels(await carRepository.getAllCars())
+      .sort(({ qtdModels: carA }, { qtdModels: carB }) => carA - carB)
+      .slice(0, numberOfBrands).map(({ brand, qtdModels }) => `${brand} - ${qtdModels}`);
+  };
 
 module.exports = {
   getBrandNamesWithMoreCarsModels,
-  getBrandNamesWithLessCarsModels
+  getBrandNamesWithLessCarsModels,
+  getNCarsBrandNamesWithMoreCarsModels,
+  getNCarsBrandNamesWithLessCarsModels
 };
