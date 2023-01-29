9afc98b7a5f2b4baa77153c16d9b221410704e22
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-01-29 10:01:12 -0300

    feat: add createOrder function in Repository

--- backend-nodejs/modulo-1/desafio/src/repository/orders.repository.js
+++ backend-nodejs/modulo-1/desafio/src/repository/orders.repository.js
@@ -1,8 +1,23 @@
 const config = require('../config/config');
-const { fileReader } = require('../utils/io');
+const { fileReader, fileWriter } = require('../utils/io');
 
 const getOrders = async () => fileReader.readFile(config['db-pedidos-path']);
 
+const createOrder = async ({ client, product, price }) => {
+  const ordersData = await getOrders();
+  const newOrderData = {
+    id: ordersData.nextId++,
+    cliente: client,
+    produto: product,
+    valor: price,
+    entregue: false,
+    timestamp: new Date(),
+  };
+  ordersData.pedidos.push(newOrderData);
+  await fileWriter.writeFile(config['db-pedidos-path'], ordersData);
+};
+
 module.exports = {
   getOrders,
+  createOrder,
 };
