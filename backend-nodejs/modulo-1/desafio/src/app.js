adadda2308acf3c8e1e87e935533964adcd74a7f
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-01-29 10:43:05 -0300

    feat: add handleError middleware in app's route

--- backend-nodejs/modulo-1/desafio/src/app.js
+++ backend-nodejs/modulo-1/desafio/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { orderRouter } = require('./routes');
+const { handleError } = require('./middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/orders', orderRouter);
+
+app.use(handleError);
 
 module.exports = app;
