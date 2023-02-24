f42f21ad3db7e53087eec02ff1d7bcf2ea1bca90
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-02-24 11:07:40 -0300

    style: remove unused comments

--- backend-nodejs/modulo-3/trabalho-pratico/src/app.ts
+++ backend-nodejs/modulo-3/trabalho-pratico/src/app.ts
@@ -7,16 +7,6 @@ const app = express();
 app.use(express.json());
 
 app.use(routers);
-// import { Animal } from './models/Animal';
-// import { Owner } from './models/Owner';
-
-// const owner = new Owner();
-// owner.findAll().then(result => console.log(result));
-
-// const animals = new Animal();
-// animals.findAll().then(result => console.log(result))
-
-
 
 export default app;
 
