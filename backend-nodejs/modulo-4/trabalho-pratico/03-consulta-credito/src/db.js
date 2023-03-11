7bc527100935e444c05b4a6d9e07b5abe2439ea0
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-03-11 14:05:06 -0300

    feat: list all clients

---
+++ backend-nodejs/modulo-4/trabalho-pratico/03-consulta-credito/src/db.js
@@ -0,0 +1,70 @@
+const Sequelize = require('sequelize');
+
+const config = {
+  dialect: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: process.env.POSTGRES_PORT || 49153,
+  database: process.env.POSTGRES_DB || 'consulta_credito',
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'mysecretpassword',
+  logging: false,
+};
+
+const sequelize = new Sequelize(config);
+
+const clienteModel = (sequelizeCliente, DataTypes) => {
+  const Cliente = sequelizeCliente.define('Cliente', {
+    CPF: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
+    Nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  });
+
+  return Cliente;
+};
+
+const consultaModel = (sequelizeConsulta, DataTypes) => {
+  const Consulta = sequelizeConsulta.define('Consulta', {
+    Valor: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    NumPrestacoes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Juros: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    Montante: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    Prestacoes: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return Consulta;
+};
+
+const cliente = clienteModel(sequelize, Sequelize.DataTypes);
+const consulta = consultaModel(sequelize, Sequelize.DataTypes);
+
+cliente.hasMany(consulta, { as: 'Consultas' });
+consulta.belongsTo(cliente);
+
+module.exports = {
+  cliente,
+  consulta,
+  sequelize,
+};
